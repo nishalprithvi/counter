848d6ddba4660aaadbc2c3d076d7a4160174fcf3
prithvinishal <nishalprithvi@gmail.com>
2024-07-26 17:58:29 +0530

    temperature conversion program

---
+++ temperature_convertor/index.js
@@ -0,0 +1,26 @@
+// Temperature Conversion Program
+
+const textBox = document.getElementById("textBox");
+const toFarenheit = document.getElementById("toFarenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+
+    if(toFarenheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "F";
+    }
+
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "C";
+    }
+
+    else{
+        result.textContent = "Select a Unit please !!";
+    }
+}
